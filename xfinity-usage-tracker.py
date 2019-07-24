184d67dcd877525c70f7f65b72b5aaf0b4afb614
Nicolas Bonamy <nicolas@bonamy.fr>
2019-07-23 22:27:53 -0500

    update redirects to usage

--- xfinity-usage-tracker.py
+++ xfinity-usage-tracker.py
@@ -32,7 +32,7 @@ def finish(args, usageData, sheetUrl):
 		if args.json:
 			print('Content-Type: application/json')
 		elif sheetUrl:
-			print('Location: {0}'.format(sheetUrl))
+			print('Location: usage.html')
 		print()
 
 	# now content
